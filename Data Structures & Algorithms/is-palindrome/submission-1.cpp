3cf45b75cac226dca8238e4b4ee1f8008a87a299
Ashfak Azad Nafi <139957665+Nafi14078@users.noreply.github.com>
2026-07-06 22:51:09 +0600

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        vector<char>v1;
+        vector<char>v2;
+        for(int i=0;i<s.size();i++){
+            if((s[i]>='a'&& s[i]<='z') || (s[i]>='A'&& s[i]<='Z') || (s[i]>='0'&& s[i]<='9')){
+                v1.push_back(tolower(s[i]));
+            }
+            else{
+                continue;
+            }
+        }
+        v2=v1;
+        reverse(v2.begin(),v2.end());
+        if(v1==v2){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
